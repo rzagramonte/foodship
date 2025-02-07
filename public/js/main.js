d3426aa826d12675aa5d7f6dc70de4eaa8a3325c
Roosiel <rzagramonte@outlook.com>
2025-02-07 15:38:09 -0500

    fixing chrome/safari iOS vh

--- public/js/main.js
+++ public/js/main.js
@@ -577,3 +577,8 @@ eventSystemMessage?.forEach((e) => {
     })}`
   );
 });
+
+window.onresize = function() {
+  document.body.height = window.innerHeight;
+}
+window.onresize(); // called to initially set the height.
